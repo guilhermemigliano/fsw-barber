3410157c66843ece1e4989b3b09971b0ed4cc3d7
Guilherme Migliano <44805868+guilhermemigliano@users.noreply.github.com>
2024-08-09 10:20:19 -0300

    chore: add next auth setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,10 +1,18 @@
-import { CalendarIcon, HomeIcon, LogOutIcon } from 'lucide-react'
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from 'lucide-react'
 import { Button } from './ui/button'
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from './ui/sheet'
 import { Avatar, AvatarImage } from './ui/avatar'
 import Link from 'next/link'
 import { quickSearchOptions } from '../_constants/search'
 import Image from 'next/image'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from './ui/dialog'
 
 const SidebarSheet = () => {
   return (
@@ -13,15 +21,35 @@ const SidebarSheet = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="p-5 border-b border-solid flex items-center gap-3">
-        <Avatar>
+      <div className="p-5 border-b border-solid flex items-center justify-between gap-3">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size={'icon'}>
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta no google
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant={'outline'} className="gap-1 font-bold">
+              <Image src="/google.svg" width={18} height={18} alt="google" />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage src="https://avatars.githubusercontent.com/u/44805868?v=4" />
         </Avatar>
 
         <div className="">
           <p className="font-bold">Guilherme Migliano</p>
           <p className="text-xs">guilherme@email.com.br</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="p-5 flex flex-col gap-2 border-b border-solid">
