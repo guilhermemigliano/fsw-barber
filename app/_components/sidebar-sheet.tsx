6876489e234dd88642d7125284961a35d60b37be
Guilherme Migliano <44805868+guilhermemigliano@users.noreply.github.com>
2024-08-09 11:09:12 -0300

    feat: add sign in with google

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from 'lucide-react'
 import { Button } from './ui/button'
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from './ui/sheet'
@@ -13,8 +15,18 @@ import {
   DialogTitle,
   DialogTrigger
 } from './ui/dialog'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 const SidebarSheet = () => {
+  const { data } = useSession()
+  const handleLoginWithGoogleClick = async () => {
+    await signIn('google')
+  }
+
+  const handleLogoutClick = async () => {
+    await signOut()
+  }
+
   return (
     <SheetContent className="overflow-y-auto">
       <SheetHeader>
@@ -22,34 +34,50 @@ const SidebarSheet = () => {
       </SheetHeader>
 
       <div className="p-5 border-b border-solid flex items-center justify-between gap-3">
-        <h2 className="font-bold">Olá, faça seu login!</h2>
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button size={'icon'}>
-              <LogInIcon />
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="w-[90%]">
-            <DialogHeader>
-              <DialogTitle>Faça login na plataforma</DialogTitle>
-              <DialogDescription>
-                Conecte-se usando sua conta no google
-              </DialogDescription>
-            </DialogHeader>
-            <Button variant={'outline'} className="gap-1 font-bold">
-              <Image src="/google.svg" width={18} height={18} alt="google" />
-              Google
-            </Button>
-          </DialogContent>
-        </Dialog>
-        {/* <Avatar>
-          <AvatarImage src="https://avatars.githubusercontent.com/u/44805868?v=4" />
-        </Avatar>
+        {data?.user ? (
+          <div className="flex items-center gap-2">
+            <Avatar>
+              <AvatarImage src={data?.user?.image ?? ''} />
+            </Avatar>
 
-        <div className="">
-          <p className="font-bold">Guilherme Migliano</p>
-          <p className="text-xs">guilherme@email.com.br</p>
-        </div> */}
+            <div className="">
+              <p className="font-bold">{data.user.name}</p>
+              <p className="text-xs">{data.user.email}</p>
+            </div>
+          </div>
+        ) : (
+          <>
+            <h2 className="font-bold">Olá, faça seu login!</h2>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button size={'icon'}>
+                  <LogInIcon />
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="w-[90%]">
+                <DialogHeader>
+                  <DialogTitle>Faça login na plataforma</DialogTitle>
+                  <DialogDescription>
+                    Conecte-se usando sua conta no google
+                  </DialogDescription>
+                </DialogHeader>
+                <Button
+                  variant={'outline'}
+                  className="gap-1 font-bold"
+                  onClick={handleLoginWithGoogleClick}
+                >
+                  <Image
+                    src="/google.svg"
+                    width={18}
+                    height={18}
+                    alt="google"
+                  />
+                  Google
+                </Button>
+              </DialogContent>
+            </Dialog>
+          </>
+        )}
       </div>
 
       <div className="p-5 flex flex-col gap-2 border-b border-solid">
@@ -86,7 +114,11 @@ const SidebarSheet = () => {
       </div>
 
       <div className="p-5 flex flex-col gap-2 border-b border-solid">
-        <Button className="justify-start gap-2" variant={'ghost'}>
+        <Button
+          className="justify-start gap-2"
+          variant={'ghost'}
+          onClick={handleLogoutClick}
+        >
           <LogOutIcon size={18} />
           Sair da coonta
         </Button>
